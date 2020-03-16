797b6187570dda4b23ce332b4acf7cade3edf78e
DiegoRSM03 <rodrigo.dsanchez@yahoo.com.ar>
2020-03-16 13:15:38 -0300

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Login</title>
+</head>
+<body>
+    <form action="mk_cookie" method="post">
+        
+    </form>
+</body>
+</html>
