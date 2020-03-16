eceb02f2a18d5654aa2508b3641c58661fee0d0c
DiegoRSM03 <rodrigo.dsanchez@yahoo.com.ar>
2020-03-16 13:50:48 -0300

    Fuentes de iconos para formulario añadidas

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="iconfont/flaticon.css">
+
     <title>User Login</title>
 </head>
 <body>
     <form action="mk_cookie" method="post">
-        
+    
     </form>
 </body>
 </html>
